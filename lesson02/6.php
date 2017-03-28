ed9a255e9555d602b8f07e0fa321fb09fa1f355b
Evgeny Belkin <sunafajro@gmail.com>
2017-03-28 08:41:11 +0300

    доработка оформления 6 и 7 заданий второго урока

--- lesson02/6.php
+++ lesson02/6.php
@@ -1,14 +1,84 @@
-<?php
+<!DOCTYPE html>
+    <head>
+        <meta http-equiv=Content-Type content="text/html;charset=UTF-8">
+        <title>Задание 6</title>
+        <style>
+        .margin-left {
+            margin-left: 55px;
+        }
+        </style>
+    </head>
+        <body>
+        <p class="margin-left"><a href="index.php">Назад</a></p>
+        <p class="margin-left"><b>Описание и код задания:</b></p>
+        <pre>
+        /* 
+        *  задание 6 
+        *  С помощью рекурсии организовать функцию возведения числа в степень.  
+        *  Формат: function getPower($val, $pow), где $val – заданное число, $pow – степень.
+        */
+        
+        $value = NULL;
+        $power = NULL;
+        
+        if(isset($_POST['value']) && isset($_POST['power'])) {
+            $value = $_POST['value'];
+            $power = $_POST['power'];
+        }
+        
+        function getPower($val, $pow) {
+          if ($pow > 1) {
+            return $val * pow($val, $pow - 1);
+          } else if ($pow == 0) {
+            return 1;
+          } else {
+            return $val;
+          }
+        }
 
-function power($val, $pow) {
-  if ($pow > 1) {
-    return $val * pow($val, $pow - 1);
-  } else if ($pow == 0) {
-  	return 1;
-  } else {
-    return $val;
-  }
-}
+        &lt;form method="post" action="6.php"&gt;
+            &lt;label&gt;Число:&lt;/label&gt;
+            &lt;input type="text" value="&lt;?= $value ?&gt;" name="value" required&gt;
+            &lt;label&gt;Степень:&lt;/label&gt;
+            &lt;input type="text" value="&lt;?= $power ?&gt;" name="power" required&gt;
+            &lt;input type="submit"&gt;
+        &lt;/form&gt;
+        &lt;p&gt;
+        &lt;?= getPower($value, $power); ?&gt;
+        &lt;/p&gt;
+        </pre>
+    
+        <p class="margin-left"><b>Результат:</b></p>        
+        <?php
 
-echo power(3, 2);
-?>
+		$value = 0;
+		$power = 0;
+        
+        if(isset($_POST['value']) && isset($_POST['power'])) {
+            $value = $_POST['value'];
+			$power = $_POST['power'];
+        }
+		
+        function getPower($val, $pow) {
+          if ($pow > 1) {
+            return $val * pow($val, $pow - 1);
+          } else if ($pow == 0) {
+            return 1;
+          } else {
+            return $val;
+          }
+        }
+        ?>
+		
+		<form class="margin-left" method="post" action="6.php">
+			<label>Число:</label>
+            <input type="text" value="<?= $value ?>" name="value" required>
+			<label>Степень:</label>
+			<input type="text" value="<?= $power ?>" name="power" required>
+            <input type="submit">
+        </form>
+		<p class="margin-left">
+        <?= getPower($value, $power); ?>
+        </p>
+    </body>
+</html>
--- lesson02/7.php
+++ lesson02/7.php
@@ -1,30 +1,119 @@
-<?php
+<!DOCTYPE html>
+    <head>
+        <meta http-equiv=Content-Type content="text/html;charset=UTF-8">
+        <title>Задание 7</title>
+        <style>
+        .margin-left {
+            margin-left: 55px;
+        }
+        </style>
+    </head>
+        <body>
+        <p class="margin-left"><a href="index.php">Назад</a></p>
+        <p class="margin-left"><b>Описание и код задания:</b></p>
+        <pre>
+        /* 
+        *  задание 7 
+        *  Написать функцию, которая вычисляет текущее время и возвращает   
+        *  его в формате с правильными склонениями, например:
+        * 
+        *  22 часа 15 минут
+        *  21 час 43 минуты
+        */
 
-function getCurTime($h, $m) {
+        $hours = 0;
+        $minutes = 0;
+        
+        if(isset($_POST['hours']) && isset($_POST['minutes'])) {
+            $hours = $_POST['hours'];
+            $minutes = $_POST['minutes'];
+        }
+        
+        function getCurTime($h, $m) {
 
-	$arr1 = [1,21];
-	$arr2 = [2,3,4,22,23];
+            $arr1 = [1,21];
+            $arr2 = [2,3,4,22,23];
 
-	$str = '';
-	if(in_array($h, $arr1)) {
-		$str .= $h . ' час ';
-	} else if(in_array($h, $arr2)) {
-		$str .= $h . ' часа ';
-	} else {
-		$str .= $h . ' часов ';
-	}
+            $str = '';
+            if(in_array($h, $arr1)) {
+                $str .= $h . ' час ';
+            } else if(in_array($h, $arr2)) {
+                $str .= $h . ' часа ';
+            } else {
+                $str .= $h . ' часов ';
+            }
 
-    if(in_array($m, $arr1)) {
-		$str .= $m . ' минута';
-	} else if(in_array($m, $arr2)) {
-		$str .= $m . ' минуты';
-	} else {
-		$str .= $m . ' минут';
-	}
+            if(in_array($m, $arr1)) {
+                $str .= $m . ' минута';
+            } else if(in_array($m, $arr2)) {
+                $str .= $m . ' минуты';
+            } else {
+                $str .= $m . ' минут';
+            }
 
 
-	return $str;
-}
+            return $str;
+        }
 
-echo getCurTime(15,20); 
-?>
+        &lt;form class="margin-left" method="post" action="7.php"&gt;
+            &lt;label&gt;Часы:&lt;/label&gt;
+            &lt;input type="text" value="&lt;?= $hours ?&gt;" name="hours" required&gt;
+            &lt;label&gt;Минуты:&lt;/label&gt;
+            &lt;input type="text" value="&lt;?= $minutes ?&gt;" name="minutes" required&gt;
+            &lt;input type="submit"&gt;
+        &lt;/form&gt;
+        &lt;p&gt;
+        &lt;?= getCurTime($hours, $minutes); ?&gt;
+        &lt;/p&gt;
+        </pre>
+    
+        <p class="margin-left"><b>Результат:</b></p>
+        <?php
+        
+		$hours = 0;
+		$minutes = 0;
+		
+        if(isset($_POST['hours']) && isset($_POST['minutes'])) {
+            $hours = $_POST['hours'];
+			$minutes = $_POST['minutes'];
+        }
+		
+        function getCurTime($h, $m) {
+
+            $arr1 = [1,21];
+            $arr2 = [2,3,4,22,23];
+
+            $str = '';
+            if(in_array($h, $arr1)) {
+                $str .= $h . ' час ';
+            } else if(in_array($h, $arr2)) {
+                $str .= $h . ' часа ';
+            } else {
+                $str .= $h . ' часов ';
+            }
+
+            if(in_array($m, $arr1)) {
+                $str .= $m . ' минута';
+            } else if(in_array($m, $arr2)) {
+                $str .= $m . ' минуты';
+            } else {
+                $str .= $m . ' минут';
+            }
+
+
+            return $str;
+        }
+
+        ?>
+		<form class="margin-left" method="post" action="7.php">
+			<label>Часы:</label>
+            <input type="text" value="<?= $hours ?>" name="hours" required>
+			<label>Минуты:</label>
+			<input type="text" value="<?= $minutes ?>" name="minutes" required>
+            <input type="submit">
+        </form>
+		<p class="margin-left">
+        <?= getCurTime($hours, $minutes); ?>
+        </p>
+    </body>
+</html>
