4a98aac7ad3c46a9c70ba6b892faa67a5caa96f6
Evgeny Belkin <sunafajro@gmail.com>
2017-03-30 14:19:35 +0300

    доработки оформления

--- index.php
+++ index.php
@@ -1,116 +1,108 @@
 <!DOCTYPE html>
-	<head>
-	    <meta http-equiv=Content-Type content="text/html;charset=UTF-8">
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <title>Курс PHP1</title>
-		<style>
-			.title {
-				font-size: 18px;
-				font-weight: bold;
-			}
-			.description {
-				font-size: 16px;
-				font-style: italic;
-			}
-			.homework {
-				font-size: 16px;
-			}
-			.margin-left {
-				margin-left: 55px;
-			}
-		</style>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
 	</head>
 	<body>
-		<h1 class="margin-left">Описание курса PHP1</h1>
-		<p>
-		<span class="title margin-left">Урок 1. Введение в PHP</span>
-		<ul class="description margin-left">
-			<li>Принципы работы динамических сайтов</li>
-			<li>Принципы работы веб-серверов</li>
-			<li>Подготовка рабочей среды</li>
-			<li>Hello, world!</li>
-			<li>Базовые конструкции языка – дескрипторы, переменные, типы данных</li>
-			<li>Версии языка и их различия на базовом уровне</li>
-		</ul>
-		<span class="homework margin-left"><a href="lesson01/">Домашнее задание</a></span><br/>
-		</p>
-		<p>
-		<span class="title margin-left">Урок 2. Условные блоки, ветвление функции</span>
-		<ul class="description margin-left">
-			<li>Принципы ветвления, визуализация, блок-схемы</li>
-			<li>Операторы if, if-else</li>
-			<li>Оператор switch</li>
-			<li>Тернарный оператор</li>
-			<li>Реализация схем логики ветвления</li>
-			<li>Функции, рекурсия</li>
-			<li>Использование функций и рекурсии для решения задач</li>
-			<li>Области видимости переменных</li>
-		</ul>
-		<span class="homework margin-left"><a href="lesson02/">Домашнее задание</a></span><br/>
-		</p>
-		<p>
-		<span class="title margin-left">Урок 3. Циклы и массивы</span>
-		<ul class="description margin-left">
-			<li>Понятие цикла, типы циклов в PHP</li>
-			<li>While, do…while, For, Foreach</li>
-			<li>Бесконечный цикл и выход из шагов цикла</li>
-			<li>Понятие массива, типы массивов в PHP</li>
-			<li>Применение циклов для работы с массивами</li>
-			<li>Многомерные массивы</li>
-			<li>Основные функции работы с массивами</li>
-			<li>Применение функции для работы с массивами</li>
-			<li>Предопределённые массивы</li>
-		</ul>
-		<span class="homework margin-left"><a href="lesson03/">Домашнее задание</a></span><br/>
-		</p>
-		<p>
-		<span class="title margin-left">Урок 4. Работа с файлами</span><br/>
-		<ul class="description margin-left">
-			<li>Файловая система и адресация, примеры на базе разных ОС</li>
-			<li>Подключение файлов с кодом</li>
-			<li>Базовые операции работы с файлами – чтение, запись.</li>
-			<li>Использование базовых операций для решения повседневных задач – логирование, чтение данных, кэширование</li>
-			<li>Типовые форматы хранения данных</li>
-		</ul>
-		<span class="homework margin-left"><a href="lesson04/">Домашнее задание</a></span><br/>
-		</p>
-		<p>
-		<span class="title margin-left">Урок 5. Базы данных MySQL и работа с ними на уровне PHP</span><br/>
-		<ul class="description margin-left">
-			<li>Создание БД, пользователей</li>
-			<li>Создание таблиц</li>
-			<li>Синтаксис базовых запросов.</li>
-			<li>Создаем, редактируем и удаляем таблицы</li>
-			<li>Изучаем 4 главных команды: INSERT, UPDATE, DELETE, SELECT</li>
-			<li>Выборка из БД и вывод на экран</li>
-		</ul>
-		</p>
-		<p>
-		<span class="title margin-left">Урок 6. Интерактивность: Методы передачи данных GET и POST, работа с формами и пользовательскими данными</span><br/>
-		<ul class="description margin-left">
-			<li>Основные методы передачи данных на сервер</li>
-			<li>Методы GET и POST</li>
-			<li>Передача данных из формы в скрипт</li>
-			<li>Обработка и сохранение данных в БД, обеспечение безопасности.</li>
-			<li>Реализация CRUD-пакета действий</li>
-			<li>Загрузка файлов на сервер</li>
-		</ul>
-		</p>
-		<p>
-		<span class="title margin-left">Урок 7. Авторизация и аутентификация</span><br/>
-		<ul class="description margin-left">
-			<li>Знакомство с понятиями авторизации и аутентификации</li>
-			<li>Сессии и Cookies</li>
-			<li>Шифрование пользовательских данных</li>
-			<li>Реализации аутентификации пользователя на сайте</li>
-		</ul>
-		</p>
-		<p>
-		<span class="title margin-left">Урок 8. Практика</span>
-		<ul class="description margin-left">
-			<li>Написание недостающей логики страниц</li>
-			<li>Создание тестовых заказов</li>
-			<li>Тестирование</li>
-		</ul>
-		</p>
+	    <div class="container">
+            <h1>Описание курса PHP1</h1>
+            <div>
+                <h3>Урок 1. Введение в PHP</h3>
+                <ul class="task-description">
+                    <li>Принципы работы динамических сайтов</li>
+                    <li>Принципы работы веб-серверов</li>
+                    <li>Подготовка рабочей среды</li>
+                    <li>Hello, world!</li>
+                    <li>Базовые конструкции языка – дескрипторы, переменные, типы данных</li>
+                    <li>Версии языка и их различия на базовом уровне</li>
+                </ul>
+                <p class="homework"><a href="./lesson01/">Домашнее задание</a></p>
+            </div>
+            <div>
+                <h3>Урок 2. Условные блоки, ветвление функции</h3>
+                <ul class="task-description">
+                    <li>Принципы ветвления, визуализация, блок-схемы</li>
+                    <li>Операторы if, if-else</li>
+                    <li>Оператор switch</li>
+                    <li>Тернарный оператор</li>
+                    <li>Реализация схем логики ветвления</li>
+                    <li>Функции, рекурсия</li>
+                    <li>Использование функций и рекурсии для решения задач</li>
+                    <li>Области видимости переменных</li>
+                </ul>
+                <p class="homework"><a href="./lesson02/">Домашнее задание</a></p>
+            </div>
+            <div>
+                <h3>Урок 3. Циклы и массивы</h3>
+                <ul class="task-description">
+                    <li>Понятие цикла, типы циклов в PHP</li>
+                    <li>While, do…while, For, Foreach</li>
+                    <li>Бесконечный цикл и выход из шагов цикла</li>
+                    <li>Понятие массива, типы массивов в PHP</li>
+                    <li>Применение циклов для работы с массивами</li>
+                    <li>Многомерные массивы</li>
+                    <li>Основные функции работы с массивами</li>
+                    <li>Применение функции для работы с массивами</li>
+                    <li>Предопределённые массивы</li>
+                </ul>
+                <p class="homework"><a href="./lesson03/">Домашнее задание</a></p>
+            </div>
+            <div>
+                <h3>Урок 4. Работа с файлами</h3>
+                <ul class="task-description">
+                    <li>Файловая система и адресация, примеры на базе разных ОС</li>
+                    <li>Подключение файлов с кодом</li>
+                    <li>Базовые операции работы с файлами – чтение, запись.</li>
+                    <li>Использование базовых операций для решения повседневных задач – логирование, чтение данных, кэширование</li>
+                    <li>Типовые форматы хранения данных</li>
+                </ul>
+                <p class="homework"><a href="./lesson04/">Домашнее задание</a></p>
+            </div>
+            <div>
+                <h3>Урок 5. Базы данных MySQL и работа с ними на уровне PHP</h3>
+                <ul class="task-description">
+                    <li>Создание БД, пользователей</li>
+                    <li>Создание таблиц</li>
+                    <li>Синтаксис базовых запросов.</li>
+                    <li>Создаем, редактируем и удаляем таблицы</li>
+                    <li>Изучаем 4 главных команды: INSERT, UPDATE, DELETE, SELECT</li>
+                    <li>Выборка из БД и вывод на экран</li>
+                </ul>
+                <p class="homework"><a href="./lesson05/">Домашнее задание</a></p>
+            </div>
+            <div>
+                <h3>Урок 6. Интерактивность: Методы передачи данных GET и POST, работа с формами и пользовательскими данными</h3>
+                <ul class="task-description">
+                    <li>Основные методы передачи данных на сервер</li>
+                    <li>Методы GET и POST</li>
+                    <li>Передача данных из формы в скрипт</li>
+                    <li>Обработка и сохранение данных в БД, обеспечение безопасности.</li>
+                    <li>Реализация CRUD-пакета действий</li>
+                    <li>Загрузка файлов на сервер</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Урок 7. Авторизация и аутентификация</h3>
+                <ul class="task-description">
+                    <li>Знакомство с понятиями авторизации и аутентификации</li>
+                    <li>Сессии и Cookies</li>
+                    <li>Шифрование пользовательских данных</li>
+                    <li>Реализации аутентификации пользователя на сайте</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Урок 8. Практика</h3>
+                <ul class="task-description">
+                    <li>Написание недостающей логики страниц</li>
+                    <li>Создание тестовых заказов</li>
+                    <li>Тестирование</li>
+                </ul>
+            </div>
+        </div>
 	</body>
 </html>
--- lesson02/index.php
+++ lesson02/index.php
@@ -7,16 +7,6 @@
 	    <title>Урок 2</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="../style.css" rel="stylesheet">
-		<style>
-			.container {
-				width:900px;
-				margin: 0 auto;
-			}
-
-			.margin-left {
-				margin-left: 55px;
-			}
-		</style>
 	</head>
 	<body>
 	    <div class="container">
