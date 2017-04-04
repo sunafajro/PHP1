f7db45af7695272182f205214600b81332f18fe5
Evgeny Belkin <sunafajro@gmail.com>
2017-04-04 16:10:38 +0300

    исправление опечаток

--- lesson05/index.php
+++ lesson05/index.php
@@ -107,7 +107,7 @@
 				<h3>Загрузить файл:</h3>
 				<form action="index.php" method="post" enctype="multipart/form-data">
 					<div class="form-group">
-						<input type="file" class="from-control" name="image" required>
+						<input type="file" class="form-control" name="image" required>
 						<p class="help-block">Файлы типа jpg, размером меньше 500 Кбайт.</p>
 					</div>
 					<input type="hidden" name="type" value="main-form">
