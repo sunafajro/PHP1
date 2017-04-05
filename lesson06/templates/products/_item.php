bc5cd32d62e2d33850e23838c32bbc18d09c4e91
Evgeny Belkin <sunafajro@gmail.com>
2017-04-05 16:23:23 +0300

    дорабатываем карточку товара

---
+++ lesson06/templates/products/_item.php
@@ -0,0 +1,27 @@
+<div class="text-center">
+    <img id="image_{{ID}}" src="{{IMG_ORIG_PATH}}" alt="{{IMG_NAME}}" class="img-thumbnail">
+</div>
+<table class="table">
+    <tbody>
+        <tr>
+            <td>Наименование:</td>
+            <td>{{NAME}}</td>
+        </tr>
+        <tr>
+            <td>Артикул:</td>
+            <td>{{ARTICUL}}</td>
+        </tr>
+        <tr>
+            <td>Описание:</td>
+            <td>{{DESCRIPTION}}</td>
+        </tr>
+        <tr>
+            <td>В наличии:</td>
+            <td>{{COUNT}}</td>
+        </tr>
+        <tr>
+            <td>Цена:</td>
+            <td>{{COST}} р.</td>
+        </tr>
+    </tbody>
+</table>
